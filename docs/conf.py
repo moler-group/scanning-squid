49e38b5a5fcda9996f48173dfc065bf434ec6958
loganbvh <logan.bvh@gmail.com>
2018-06-12 10:43:13 -0700

    Hide copyright

--- docs/conf.py
+++ docs/conf.py
@@ -83,6 +83,7 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
+html_show_copyright = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
