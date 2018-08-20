26946a92ec4992ea1ffb4cecaeccc64cb12e8b2d
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2018-08-20 08:07:25 -0700

    Update utils.py

--- scanning-squid/utils.py
+++ scanning-squid/utils.py
@@ -25,7 +25,7 @@ def load_json_ordered(filename: str) -> OrderedDict:
     """Loads json file as an ordered dict.
 
     Args:
-        filname: Path to json file to be loaded.
+        filename: Path to json file to be loaded.
 
     Returns:
         OrderedDict: odict
