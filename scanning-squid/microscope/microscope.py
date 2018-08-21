805a2c5ed3e92a3d89b1bed44f266000bf290287
BF4K-SQUID <bf4k.squid@gmail.com>
2018-08-21 08:04:36 -0700

    fix data saving if no touchdown

--- scanning-squid/microscope/microscope.py
+++ scanning-squid/microscope/microscope.py
@@ -269,9 +269,10 @@ class Microscope(Station):
         try:
             log.info('Starting capacitive touchdown.')
             loop.run()
-            if abs(old_pos[0]) < 0.002 and abs(old_pos[1]) < 0.002 and self.scanner.td_height is not None:
-                self.scanner.metadata['plane'].update({'z': self.scanner.td_height})
-            data.metadata['loop']['metadata'].update({'td_height': self.scanner.td_height})
+            if self.scanner.td_height is not None:
+                data.metadata['loop']['metadata'].update({'td_height': self.scanner.td_height})
+                if abs(old_pos[0]) < 0.002 and abs(old_pos[1]) < 0.002:
+                    self.scanner.metadata['plane'].update({'z': self.scanner.td_height})
         except KeyboardInterrupt:
             log.warning('Touchdown interrupted by user. Retracting scanner.')
             #: Set break_loop = True so that get_plane() and approach() will be aborted
